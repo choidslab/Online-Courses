86c679dda121bd18bbaf4495d67be9a152d332f0
choidslab <choids.lab@gmail.com>
2020-09-01 16:47:07 +0900

    Add Section5 - Python if statement

---
+++ Udemy/The Complete Python Programmer Bootcamp 2020/Section5/if_statement.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 16:14:39 2020
+
+@author: dooseop
+"""
+
+# some_condition = False
+
+# if some_condition:
+#     print('True')
+# else:
+#     print('False')
+
+# temp = int(input('Please Enter the temperature in Celsius: '))
+
+# if temp > 30:
+#     print('Wear shorts and sun cream!')
+# elif temp <= 30 and temp > 20:
+#     print('It\'s warm, but not shorts weather!')
+# elif temp <= 20 and temp > 10:
+#     print('You\'ll probably need a vest today!')
+# else:
+#     print('Too cold!')
+
+
+my_string = 'Python'
+
+# length of string
+print(len(my_string))
+
+# string indexing
+print(my_string[0])
+
+# string slicing
+print(my_string[0:4])
+print(my_string[:4])
