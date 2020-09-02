8807187d0484d4adf02b6515fe52e2f267859e59
choidslab <choids.lab@gmail.com>
2020-09-02 14:56:43 +0900

    Udemy Python Bootcamp 2020 - Python for loops examples

---
+++ Udemy/The Complete Python Programmer Bootcamp 2020/Section6/loops.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 14:07:11 2020
+
+@author: TimChoi
+"""
+
+# for loops
+# for i in range(10): # 'range' creates an iterator -> in this case a series of values 0~9
+#     print(i)
+
+# for i in range(10): 
+    # print(i, end=' ') # end='' -> without new line
+
+# for i in range(1, 10): # set a start num
+#     print(i, end=' ')
+ 
+# for i in range(1, 11): 
+#     print(i, end=' ')
+    
+# for i in range(0, 101, 4):
+#     print(i, end=' ')
+
+word = 'python'
+
+for char in word:
+    print(char)
