be4346b34eea3b1941b72475a059704a22263a72
choidslab <choids.lab@gmail.com>
2020-09-09 16:33:48 +0900

    Udemy Python Bootcamp 2020 - Study while loops

--- Udemy/The Complete Python Programmer Bootcamp 2020/Section6/loops.py
+++ Udemy/The Complete Python Programmer Bootcamp 2020/Section6/loops.py
@@ -24,3 +24,29 @@ Created on Wed Sep  2 14:07:11 2020
 
 # for char in word:
 #     print(char)
+
+# n = 10
+
+# while n > 0:
+#     print(n)
+#     n -= 1
+
+# user_input = int(input('Please enter ages of class member(Type -1 to end): '))
+# ages = []
+# while user_input > 0:
+#     ages.append(user_input)
+#     user_input = int(input('The next ages: '))
+
+# print('The ages are', ages)
+
+count = 0
+class_names = []
+
+name = input('Please enter name type n to stop: ')
+while name != 'n':
+    count += 1
+    class_names.append(name)
+    print(f'{name} has been added.')
+    name = input('Next name?: ')
+
+print(f'There are {count} people in the class, they are {class_names}')
