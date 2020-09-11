1868b85e92e585d0d3838a156111432c4912bb4e
choidslab <choids.lab@gmail.com>
2020-09-11 22:37:31 +0900

    Python BootCamp2020 - Python loop exercise

---
+++ Udemy/Python_BootCamp2020/Section6/loop_exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 21:37:53 2020
+
+@author: TimChoi
+"""
+
+'''
+Q1. Ask the user for two numbers between 1 and 100. 
+Then count from the lower number to the higher number. 
+Print the results to the screen.
+'''
+
+num1 = int(input('Enter a first num(1~100): '))
+num2 = int(input('Enter a second num(1~100): '))
+
+while num1 < 1 or num2 < 1 or num1 > 100 or num2 > 100 or num1 == num2:
+    # input 값을 체크 -> 유효하지 않은 경우 계속해서 사용자 입력을 받도록 함
+    print('Invalid values! Try again.')
+    num1 = int(input('Enter a first num(1~100): '))
+    num2 = int(input('Enter a second num(1~100): '))
+
+if num1 < num2:
+    for i in range(num1, num2 + 1):
+        print(i)
+else:
+    for i in range(num2, num1 + 1):
+        print(i)
